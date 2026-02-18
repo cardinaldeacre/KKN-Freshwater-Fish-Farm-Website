81343fcb3013b60f36145ce13324f20a16a29c08
cardinaldeacre <cardinaldeacre@gmail.com>
2026-02-18 21:11:31 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "flowbite-react";
+
+export default function App() {
+  return (
+    <div className="p-20 flex justify-center bg-gray-50 min-h-screen">
+      <Button color="blue" pill>
+        test! 🚀
+      </Button>
+    </div>
+  );
+}
