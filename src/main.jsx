698eb879c098b13387db69efb340292d9358906f
cardinaldeacre <cardinaldeacre@gmail.com>
2026-02-19 14:53:28 +0700

    add flowbite

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'flowbite/dist/flowbite.css';
+import 'flowbite';
 import './index.css'
 import App from './App.jsx'
 
