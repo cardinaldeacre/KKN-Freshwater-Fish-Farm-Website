54537d9df259d37669e6a65b3f500449e0a61a70
cardinaldeacre <cardinaldeacre@gmail.com>
2026-02-21 10:10:47 +0700

    fix responsive layout

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -21,24 +21,30 @@ export default function Products() {
 
     return (
         <section>
-            <div className="flex max-w-screen items-center h-24 bg-blue-950 ">
-                <h2 className="text-2xl mx-auto justify-center font-bold text-white text-center">Produk Kami</h2>
+            <div className="flex items-center h-24 bg-blue-950 shadow-inner">
+                <h2 className="text-2xl md:text-3xl mx-auto font-bold text-white text-center">
+                    Produk Kami
+                </h2>
             </div>
-            <div className="grid-cols-3 mx-auto items-center px-2 md:px2 sm:px-2 py-10 max-w-full grid gap-10">
-                {products.map((product, index) => (
-                    <Card
-                        className="max-w-sm mx-auto"
-                        imgAlt="Meaningful alt text for an image that is not purely decorative"
-                        imgSrc={images[index]}
-                    >
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {products[index].title}
-                        </h5>
-                        <p className="font-normal text-gray-700 dark:text-gray-400">
-                            {products[index].description}
-                        </p>
-                    </Card>
-                ))}
+
+            <div className="container mx-auto px-4 py-12">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+                    {products.map((product, index) => (
+                        <Card
+                            key={index}
+                            className="max-w-sm mx-auto"
+                            imgAlt="Meaningful alt text for an image that is not purely decorative"
+                            imgSrc={images[index]}
+                        >
+                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {products[index].title}
+                            </h5>
+                            <p className="font-normal text-gray-700 dark:text-gray-400">
+                                {products[index].description}
+                            </p>
+                        </Card>
+                    ))}
+                </div>
             </div>
         </section >
     )
