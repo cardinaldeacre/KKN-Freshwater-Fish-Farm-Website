9452822680a9fff61bb3e6a9c19143421e9e1e4e
cardinaldeacre <cardinaldeacre@gmail.com>
2026-02-21 22:10:00 +0700

    add new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import Activities from "@/components/Activities";
 import Hero from "@/components/Hero";
 import Products from "@/components/Products";
 import { Carousel } from "flowbite-react";
+import StepsToStart from "@/components/StepToStart";
 
 export default function Home() {
     return (
         <>
             <Hero />
             <Activities />
+            <StepsToStart />
             <Products />
         </>
     )
