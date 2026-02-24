560ff92312a186afe411f8aede34516e4191cdef
cardinaldeacre <cardinaldeacre@gmail.com>
2026-02-24 07:12:43 +0700

    init contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Button } from "flowbite-react";
 import Navbar from "./layouts/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 import Footer from "./layouts/Footer";
 import ScrollToTop from "./layouts/ScrollToTop";
 
@@ -14,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/tentang" element={<h1>Tentang</h1>} />
         <Route path="/produk" element={<h1>Produk</h1>} />
-        <Route path="/kontak" element={<h1>Kontak</h1>} />
+        <Route path="/kontak" element={<Contact />} />
       </Routes>
       <Footer />
       <ScrollToTop />
